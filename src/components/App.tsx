1a22e1f24c70eb572fd411776dafd4e06ec114c9
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-03-13 21:30:58 +0400

    feat: add slider, update styles for header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,14 +3,15 @@ import Header from './Header/Header';
 import React from "react";
 import "../css/app.scss";
 import Footer from "./Footer";
-import Countries from './Countries'
+import Countries from './Countries';
+import Slider from './Slider/Slider';
 
 const App: React.FC = () => {
   return (
     <>
       <Header/>
       <main className="main">
-        <p>Slider</p>
+        <Slider />
         <Countries />
       </main>
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, CSSProperties } from 'react';
 import Logo from './components/Logo/Logo';
 import Search from './components/Search/Search';
 import Profile from './components/Profile/Profile';
 
 const Header = () => {
+  const topHeaderPosition: CSSProperties = {
+    width: '100%',
+    position: 'absolute', 
+    backgroundColor: 'none'
+ }
+
+  const scrollHeaderPosition: CSSProperties = {
+    position: 'sticky', 
+    backgroundColor: '#222222'
+  }
+
+  const [headerStyle, setHeaderStyle] = useState(topHeaderPosition);
+
+  useEffect(()=> {
+    window.addEventListener('scroll', () => {
+      console.log('preconditional', window.pageYOffset > 0);
+      if (window.pageYOffset > 0) {
+        setHeaderStyle(scrollHeaderPosition);
+      } else if (window.pageYOffset === 0){
+        setHeaderStyle(topHeaderPosition)
+      }
+
+      console.log(window.pageYOffset, headerStyle);
+    })
+  }, [])
+
   return (
-    <header className='header'>
+    <header className='header' style={headerStyle}>
       <div className='wrapper'>
         <div className='headerContainer'>
           <Logo />
-          {/* remove for country page */}
+          {/* remove search for country page */}
           <Search /> 
           <Profile />
         </div>
