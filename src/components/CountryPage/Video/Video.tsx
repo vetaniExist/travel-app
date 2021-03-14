70290f0b6b65f1ab9a79397b5bd43ff896f7b7be
ArseniyXaoc <xaoc190789@gmail.com>
2021-03-14 19:12:07 +0300

    feat: add video component options

--- src/components/CountryPage/Video/Video.tsx
+++ src/components/CountryPage/Video/Video.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+// @ts-ignore
+import { gapi } from "gapi-script";
 
+export default function Video() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [videos, setVideos] = useState("");
 
-function Video() {
+  const arrSearch = {
+    part: [
+      "snippet",
+    ],
+    maxResults: 5,
+    q: "Paris attractions",
+  };
+
+  const videoOpts = {
+    height: "390",
+    width: "640",
+  };
+
   return (
     <>
-      <h3>Video</h3>
+        
     </>
-
   );
 }
-
-export default Video;
