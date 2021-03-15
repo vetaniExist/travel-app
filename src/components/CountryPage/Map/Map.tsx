4aad75d6d313474f7744b23a64d411e5cb2f9c85
Ekaterina Matveyeva <grinteabox@gmail.com>
2021-03-15 19:14:28 +0200

    feat: add map's height

--- src/components/CountryPage/Map/Map.tsx
+++ src/components/CountryPage/Map/Map.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./MapStyles.scss";
 
 function Map() {
   return (
-    <>
+    <div className="map">
       <h2 className="countryDetailsTitle">Location</h2>
       <div>Map</div>
-    </>
+    </div>
   );
 }
 
