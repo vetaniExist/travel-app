d950475422a55b3d454509645ef3d1da030665b4
vetaniExist <vetani3123@mail.ru>
2021-03-16 14:04:28 +0300

    feat: add yandex map

--- src/components/CountryPage/Map/Map.tsx
+++ src/components/CountryPage/Map/Map.tsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { YMaps, Map as YMap } from 'react-yandex-maps';
 import "./MapStyles.scss";
+
+/* lang=ru_RU;
+lang=en_US;
+lang=en_RU;
+lang=ru_UA;
+lang=uk_UA;
+lang=tr_TR. */
 
 function Map() {
   return (
-    <div className="map">
-      <h2 className="countryDetailsTitle">Location</h2>
-      <div>Map</div>
-    </div>
-  );
+    <YMaps
+      query={{
+        lang: "en_US"
+      }}>
+      <div>
+        My awesome application with maps!
+      <YMap
+          defaultState={{ center: [55.75, 37.57], zoom: 5 }}
+          width={"100%"}
+          height={"600px"}
+
+        />
+      </div>
+    </YMaps>
+  )
 }
 
 export default Map;
