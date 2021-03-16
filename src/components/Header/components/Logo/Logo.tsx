8f4a5ed73bb796bd5f26d65bf8e314ff247627c0
Ekaterina Matveyeva <grinteabox@gmail.com>
2021-03-16 20:11:08 +0200

    feat: add logo link

--- src/components/Header/components/Logo/Logo.tsx
+++ src/components/Header/components/Logo/Logo.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Logo = () => <div className='logo'>Travel</div>;
-
+const Logo = () => (
+  <div className='logo'>
+    <Link to="/">Travel </Link>
+  </div>
+);
 export default Logo;
+
