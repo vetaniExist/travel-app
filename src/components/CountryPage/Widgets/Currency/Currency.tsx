30bf527730cd27f8695ebed503efddcfb0195504
Ekaterina Matveyeva <grinteabox@gmail.com>
2021-03-17 14:24:48 +0200

    feat: add styles

--- src/components/CountryPage/Widgets/Currency/Currency.tsx
+++ src/components/CountryPage/Widgets/Currency/Currency.tsx
@@ -3,7 +3,15 @@ import "./CurrencyStyle.scss";
 
 function Currency() {
   return (
-    <div className="card__template currency">Currency</div>
+    <div className="card__template card__template_widget currency">
+
+      <h4>Currency</h4>
+
+      <div className="widgetTextItem">Currency</div>
+      <div className="widgetTextItem">Currency</div>
+      <div className="widgetTextItem">Currency</div>
+
+    </div>
   );
 }
 
