968cde3066c2763db4c80e71e13376bf474aba02
vetaniExist <vetani3123@mail.ru>
2021-03-19 23:48:18 +0300

    feat: add multilang description

--- src/server/data/initFullData.js
+++ src/server/data/initFullData.js
@@ -2,10 +2,14 @@ import fullData from "./fullData.json";
 
 const data = [];
 
+export const en_data = [];
+export const ru_data = [];
+export const de_data = [];
+
 fullData.forEach((country) => {
   const dataObj = {
     name: country.name,
-    description: country.description.en,
+    description: country.description,
     nativeName: country.nativeName,
     capital: country.capital,
     flag: country.flag,
@@ -20,6 +24,14 @@ fullData.forEach((country) => {
 
   };
   data[country.name.toLowerCase()] = dataObj;
+
+  en_data[country.name.toLowerCase()] = { ...dataObj };
+  ru_data[country.name.toLowerCase()] = { ...dataObj };
+  de_data[country.name.toLowerCase()] = { ...dataObj };
+
+  en_data[country.name.toLowerCase()].description = en_data[country.name.toLowerCase()].description.en;
+  ru_data[country.name.toLowerCase()].description = ru_data[country.name.toLowerCase()].description.ru;
+  de_data[country.name.toLowerCase()].description = de_data[country.name.toLowerCase()].description.de;
 });
 
 export default data;
--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -8,7 +8,8 @@ import webpackHotMiddleware from "webpack-hot-middleware";// eslint-disable-line
 import config from "../../webpack.dev.config";
 
 import countriesList from "./data/countryList.json";
-import data from "./data/initFullData";
+import data, { en_data, ru_data, de_data } from "./data/initFullData";
+console.log(en_data)
 
 const DIST_DIR = __dirname;
 const HTML_FILE = path.join(DIST_DIR, "index.html");
@@ -35,13 +36,37 @@ server.get("/api/countriesList", (req, res) => {
 
 server.get("/api/country", (req, res) => {
   const countryName = Object.keys(data)[0];
-  const countryData = data[countryName] || {};
+  const countryData = en_data[countryName] || {};
   res.json(countryData);
 });
 
 server.get("/api/country/:countryName", (req, res) => {
   const countryName = req.params.countryName.toLowerCase();
-  const countryData = data[countryName] || {};
+  const countryData = en_data[countryName] || {};
+  res.json(countryData);
+});
+
+server.get("/api/country/:countryName/:lang", (req, res) => {
+  const countryName = req.params.countryName.toLowerCase();
+  const getLangData = (countryName) => {
+    switch(req.params.lang) {
+      case "ru": {
+        return ru_data[countryName];
+      }
+      case "de": {
+        return de_data[countryName];
+      }
+      case "all": {
+        return data[countryName];
+      }
+      case "en": {
+      }
+      default: {
+        return en_data[countryName];
+      }
+    }
+  }
+  const countryData = getLangData(countryName) || {};
   res.json(countryData);
 });
 
