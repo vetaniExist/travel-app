e0b367cdb582b379e91f1bfad119fdc8c30ddbc5
vetaniExist <vetani3123@mail.ru>
2021-03-20 19:57:01 +0300

    feat: add login func

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -10,12 +10,49 @@ import config from "../../webpack.dev.config";
 import countriesList from "./data/countryList.json";
 import data, { ru_data, de_data } from "./data/initFullData";
 
+import passport from "passport";
+
+const Strategy = require('passport-local').Strategy;
+
+const db = require('./db');
+
 const DIST_DIR = __dirname;
 const HTML_FILE = path.join(DIST_DIR, "index.html");
 
 const PORT = process.env.PORT || 3000;
 
 const compiler = webpack(config);
+
+function restoreCookie(res) {
+  res.cookie("userName", "");
+}
+
+function storeCookie(req, res) {
+  res.cookie("userName", req.user.displayName);
+}
+
+passport.use(new Strategy(
+  function (username, password, cb) {
+    db.users.findByUsername(username, function (err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
 
 const server = express();
 
@@ -28,6 +65,9 @@ server.use(webpackHotMiddleware(compiler));
 server.use(cors());
 
 server.use(express.static(DIST_DIR));
+
+server.use(passport.initialize());
+server.use(passport.session());
 
 server.get("/api/countriesList", (req, res) => {
   res.json(countriesList);
@@ -65,8 +105,29 @@ server.get("/api/country/:countryName/:lang", (req, res) => {
   res.json(countryData);
 });
 
+const authenticate = passport.authenticate('local', { session: true });
+
+server.get("/getUserData", (req, res) => {
+  if (req.user) {
+    res.send({ username: req.user.displayName });
+  } else {
+    res.send({ username: "214" });
+  }
+});
+
+server.post('/login', authenticate, (req, res) => {
+  /*   storeCookie(req, res); */
+  res.send({ username: req.user.displayName });
+});
+
+server.get('/logout',
+  function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+
 server.get("/*", (req, res) => {
   res.sendFile(HTML_FILE);
 });
-
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));// eslint-disable-line
